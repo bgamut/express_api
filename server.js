7521544b09c674a10635dc68c06b984ff6dd0e54
Bernard Ahn <bgamut@gmail.com>
2025-03-25 23:38:59 +0900

    light page added

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const port = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.send("Subscribe to Arpan Neupane's channel");
 });
-
+app.get("/light", (req, res) => {
+  res.send("you have entered the light page");
+});
 app.listen(port, () => {
   `Server started on port ${port}`;
 });
