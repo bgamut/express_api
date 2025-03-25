99e0ac4b2db76dbc387fd08ca90a08b85744e1b6
Bernard Ahn <bgamut@gmail.com>
2025-03-26 01:24:38 +0900

    url change to vercel

--- req.py
+++ req.py
@@ -2,8 +2,10 @@ import requests
 import json
 # Making a PATCH request
 headers={'content-type': 'application/json'}
-#r = requests.patch('https://express-api-bice.vercel.app/update_light/1',headers=headers,data=None )
-r = requests.patch('http://localhost:8080/update_light/1',headers=headers,data=json.dumps({"power":"off"}) )
+#r = requests.patch('http://localhost:8080/update_light/1',headers=headers,data=json.dumps({"power":"off"}) )
+r = requests.patch('https://express-api-bice.vercel.app/update_light/1',headers=headers,data=json.dumps({"power":"off"}))
+
+
 # check status code for response received
 # success code - 200
 #print(r)
