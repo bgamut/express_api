30efc2995f78b80c53342455704f16d8e447c19d
Bernard Ahn <bgamut@gmail.com>
2025-03-25 23:17:17 +0900

    created light page

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const port = ProcessingInstruction.env.PORT || 8080;
 app.get('/', (req, res) => {
     res.send('Welcome to the Express.js Tutorial');
 });
-
+app.get('/light', (req, res) => {
+    res.send('test light api ');
+});
 // Start the server
 app.listen(port, () => {
     'Server started on port ${port}';
